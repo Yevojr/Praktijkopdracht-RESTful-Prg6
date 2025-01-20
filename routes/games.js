0680b6425961b2c0b610b22d5367e3be088a2897
Yevgeny Stephens <aloha.yevo@gmail.com>
2025-01-20 12:32:03 +0100

    Added routes to games.js

--- routes/games.js
+++ routes/games.js
@@ -4,3 +4,70 @@ import {faker} from "@faker-js/faker";
 
 const router = express.Router();
 
+
+router.options('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.status(204).send();
+})
+
+router.get('/', async (req, res) => {
+    const games = await Game.find();
+
+    res.json({
+        items: [games],
+        "_links": {
+            "self": {
+                "href": `${process.env.HOST}games`,
+            },
+            "collection": {
+                "href": `${process.env.HOST}games`,
+            }
+        }
+    })
+});
+
+router.options('/seed', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+    res.status(204).send();
+});
+
+router.post('/seed', async (req, res) => {
+   try
+   {
+       await Game.deleteMany({});
+       console.log(req.body.amount);
+
+       for (let i = 0; i < req.body.amount; i++) {
+           await Game.create({
+               title: faker.word.adverb(),
+               description: faker.lorem.paragraph({min: 2, max: 5}),
+               genre: faker.word.noun(),
+           });
+       }
+
+       res.json({message: `${req.body.amount} games have been added!`})
+   } catch (e) {
+       console.log(e);
+       res.status(400).send()
+   }
+})
+
+router.options('/:id', (req, res) => {
+
+});
+
+
+router.get('/:id', async (req, res) => {
+
+});
+
+router.post('/:id', async (req, res) => {
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+});
+export default router;
